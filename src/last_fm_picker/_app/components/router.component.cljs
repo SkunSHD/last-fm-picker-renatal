3053a459aa177d539d835f0fbd90c734ae28ee4f
Vladimir Sedikh <andiwillfly@dev.casino-x.com>
2018-01-19 18:40:59 +0200

    REF REF REF?

--- src/last_fm_picker/_app/components/router.component.cljs
+++ src/last_fm_picker/_app/components/router.component.cljs
@@ -2,9 +2,23 @@
 	(:require
 		[reagent.core :as r :refer [atom]]
 		[last-fm-picker.rn :refer [ReactNative app-registry text button link input view image touchable-highlight]]
+;		Models
+		[last-fm-picker._app.models.router :as router-model]
+;		Scenes
+		[last-fm-picker._app.scenes.home :as home_scene]
+		[last-fm-picker._app.scenes.search_artists :as search_artists_scene]
 ))
 
 
+(def log (.-log js/console))
+
+
+(defn render_scene []
+	(case @router-model/current_scene
+		"HomeScene" [home_scene/render]
+		"HomeScene1" [search_artists_scene/render]))
+
+
 (defn render []
-        [text "Router!"])
+	[render_scene])
 
---
+++ src/last_fm_picker/_app/scenes/home.scene.cljs
@@ -0,0 +1,8 @@
+(ns last-fm-picker._app.scenes.home
+	(:require
+		[reagent.core :as r :refer [atom]]
+		[last-fm-picker.rn :refer [ReactNative app-registry text button link input view image touchable-highlight]]
+		))
+
+(defn render []
+	[text "HOME PAGE!!!!!!!"])
