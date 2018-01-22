6e949e2a24802fc5fcc5811e26336c66c7e72270
malyutenko.d <malyutenko.d@gmail.com>
2018-01-22 13:37:59 +0200

    login1

--- src/last_fm_picker/_app/models/user.model.cljs
+++ src/last_fm_picker/_app/models/user.model.cljs
@@ -1,6 +1,9 @@
 (ns last-fm-picker._app.models.user
 	(:require
-		[reagent.core :as r :refer [atom]]) )
+		[reagent.core :as r :refer [atom]]
+;		Models
+		[last-fm-picker._app.models.search-by-username :as search-by-username-model]
+		) )
 
 (def log (.-log js/console))
 
@@ -8,13 +11,22 @@
 (def user (atom nil))
 (def isAuthorized (atom false))
 
+
 (defn set_username [text]
 	(reset! user text))
 
+
 (defn loginIn []
-	(reset! isAuthorized true))
+	(reset! isAuthorized true)
+	(search-by-username-model/set_current_search @user)
+	(search-by-username-model/get_user_artists)
+	)
+
 
 (defn loginOut []
 	(reset! user nil)
-	(reset! isAuthorized false))
+	(reset! isAuthorized false)
+	(search-by-username-model/set_current_search "")
+	(search-by-username-model/set_current_search_artists [])
+	)
 
