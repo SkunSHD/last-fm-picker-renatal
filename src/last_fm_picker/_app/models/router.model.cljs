0859f641e3949efc95afaf788b611d6cedf90de1
Vladimir Sedikh <andiwillfly@dev.casino-x.com>
2018-01-22 13:25:16 +0200

    Fix 35667

--- src/last_fm_picker/_app/models/router.model.cljs
+++ src/last_fm_picker/_app/models/router.model.cljs
@@ -3,6 +3,8 @@
 		[reagent.core :as r :refer [atom]]) )
 
 (def log (.-log js/console))
+(def group (.-log js/console))
+(def groupEnd (.-log js/console))
 
 
 (def current_scene (atom "HomeScene"))
@@ -15,5 +17,6 @@
 ; ========
 ; Watchers
 (defn- -on_current_scene_change [key atom old new]
-	(log "current_scene_change " old " -> " new))
+	(groupEnd)
+	(group old " -> " new))
 (add-watch current_scene "current_scene_change" -on_current_scene_change)
